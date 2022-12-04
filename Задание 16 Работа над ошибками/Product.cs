965682456edce58c7f151b593f44d95ad5d78a97
MIKHAIL <MIKHAIL@DESKTOP-6MU8OHN>
2022-12-04 20:49:12 +0500

    Добавьте файлы проекта.

---
+++ Задание 16 Работа над ошибками/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Задание_16_Работа_над_ошибками
+
+{
+    class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
